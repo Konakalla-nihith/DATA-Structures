db37f7b9a17031d51bd0de2d80c85dcd4d719daa
Konakalla-nihith <85787279+Konakalla-nihith@users.noreply.github.com>
2021-11-25 16:14:56 +0530

    Add files via upload

---
+++ duplicates.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class duplicates
+{
+  public static void main (String[]args)
+  {
+    Scanner s = new Scanner (System.in);
+    int ele, n;
+      System.out.println ("Enter the number of elemets : ");
+      n = s.nextInt ();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++)
+      {
+	a[i] = s.nextInt ();
+      }
+    for (int j = 0; j < n; j++)
+      {
+	for (int k = j + 1; k < n; k++)
+	  {
+	    if (a[j] == a[k])
+	      {
+		System.out.println (a[j]);
+		break;
+	      }
+	  }
+      }
+  }
+}
